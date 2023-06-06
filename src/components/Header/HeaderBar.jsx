c15495ae32ee29f6de03773e3f1cc9b90fbbbf08
Anadi Thakur <anadithakur99@gmail.com>
2023-06-06 12:51:22 +0530

    chaned one file name casing

--- src/components/Header/HeaderBar.jsx
+++ src/components/Header/HeaderBar.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { TbSearch } from "react-icons/tb";
 import { CgShoppingCart } from "react-icons/cg";
 import { AiOutlineHeart } from "react-icons/ai";
-import "./Header.scss";
+import "./header.scss";
 import Search from "./Search/Search";
 import { Context } from "../../utils/context";
 import Cart from "../Cart/Cart";
